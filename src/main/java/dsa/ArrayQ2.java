c77d21d1ca34e3e2ff3c07a605d6bcec2638a21f
jyotik16 <jyotik1608@gmail.com>
2023-07-18 23:10:33 +0530

    sort-reverse pairs

--- src/main/java/dsa/ArrayQ2.java
+++ src/main/java/dsa/ArrayQ2.java
@@ -9,7 +9,11 @@ public class ArrayQ2 {
         int [] B={3,11,-1,5}; //{-1,3,5,11}
         int [] C={5, 17, 100, 11};
        // magicNumber(C);
-        absolouteNumber(C);
+        //absolouteNumber(C);
+        int [] D={1, 3, 2};
+       // inversionCount(D);
+        int [] E={1, 3, 2, 3, 1};
+        reversePairs(E);
     }
 
         public static int solve(int[] A) {
@@ -183,5 +187,110 @@ public class ArrayQ2 {
         System.out.println("minMagicNumber "+minMagicNumber);
     }
 
+    static int mod = 1000000007;
+    static long inversionCount = 0l;
+    public static int inversionCount(int[] A) {
+        int n = A.length;
+        mergeSort(A,0,n-1);
+        return (int) inversionCount;
+    }
+
+    public static void mergeSort(int[] A, int s, int e){
+        if(s >= e) return;
+        int mid = (s+e)/2;
+        mergeSort(A,s,mid);
+        mergeSort(A,mid+1,e);
+        merge(A,s,mid,e);
+    }
+
+    public static void merge(int[] A, int s, int mid, int e){
+        int n1 = mid - s + 1; //number of elements in array1
+        int n2 = e - mid;//number of elements in array2
+        int A1[] = new int[n1];
+        int A2[] = new int[n2];
+        int index = 0;
+
+        //filling A1 and A2
+        for(int i = s; i <= mid; i++)
+            A1[index++] = A[i];
+
+        index = 0;
+        for(int i = mid+1; i <= e; i++)
+            A2[index++] = A[i];
+
+        int i = 0, j = 0; // i and j referncing A1 and A2 array respectively
+        index = s; // important
+
+        while(i < n1 && j < n2){
+            if(A1[i] <= A2[j]) // equals to condition for equal elements
+                A[index++] = A1[i++];
+            else{
+                inversionCount = (inversionCount + (A1.length - i))%mod; //here fulfilling problem statement condition
+                A[index++] = A2[j++];
+            }
+        }
+        //if some elements still remaining after traversal
+        if(i < n1){
+            while(i < n1){
+                A[index++] = A1[i++];
+            }
+        }else{
+            while(j < n2){
+                A[index++] = A2[j++];
+            }
+        }
+    }
+
+    public static void reversePairs(int[] A){
+        int n = A.length;
+        mergeSort2(A,0,n-1);
+        System.out.println("reverse pairs:"+count);
+    }
+    public static int count = 0;
+    public static void mergeSort2(int[] A, int s, int e){
+        if(s >= e) return;
+        int mid = (s+e)/2;
+        mergeSort(A,s,mid);
+        mergeSort(A,mid+1,e);
+        merge2(A,s,mid,e);
+    }
+    public static void merge2(int[] A, int s, int mid, int e){
+        int n1 = mid - s + 1;
+        int n2 = e - mid;
+        int A1[] = new int[n1];
+        int A2[] = new int[n2];
+        int index = 0;
+
+        for(int i = s; i <= mid; i++)
+            A1[index++] = A[i];
+
+        index = 0;
+        for(int i = mid+1; i <= e; i++)
+            A2[index++] = A[i];
+
+        int i = 0, j = 0;
+        index = s; // important
+
+        while(i < n1 && j < n2){
+            if(A1[i] <= A2[j]) // equals to condition for equal elements
+                A[index++] = A1[i++];
+            else{
+                    if (A1[i] >= 2*A2[j]) {
+                        count = (count + (A1.length - i)) % mod;
+                    }
+                A[index++] = A2[j++];
+            }
+        }
+        //if some elements still remaining after traversal
+        if(i < n1){
+            while(i < n1){
+                A[index++] = A1[i++];
+            }
+        }else{
+            while(j < n2){
+                A[index++] = A2[j++];
+            }
+        }
+    }
 }
 
